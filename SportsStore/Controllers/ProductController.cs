071b785540421bd13a31bfdb23ba812640cd1bd6
Mark Carringer <mark_carringer@yahoo.com>
2017-06-15 19:51:33 -0400

    Starting Chapter 9.

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -16,10 +16,11 @@ namespace SportsStore.Controllers
         }
 
         // the List action method
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
             => View(new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((page - 1) * PageSize)
                     .Take(PageSize),
@@ -28,7 +29,8 @@ namespace SportsStore.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             });
     }
 }
